4bdcd9bbdd555b41d7e572565fdfa078b3b0ec01
Alberto Gonella <albygonella@gmail.com>
2022-11-11 13:08:34 +0100

    cambiato nome form

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace newRepoGonellaAlberto
 {
-    public partial class Form1 : Form
+    public partial class frmProvaGitHub : Form
     {
-        public Form1()
+        public frmProvaGitHub()
         {
             InitializeComponent();
         }
