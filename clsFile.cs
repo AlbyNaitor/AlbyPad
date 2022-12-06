768563eee26fe55aeeb9024fa7843593a471a7e2
Alberto Gonella <albygonella@gmail.com>
2022-12-06 21:47:08 +0100

    Implementata gestione salvataggio e caricamento .rtf

---
+++ clsFile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.IO;
+using System.Windows.Forms; // per le finestre di dialogo
+
+namespace newRepoGonellaAlberto
+{
+    class clsFile
+    {
+        private string filePath;
+        private bool modificato;
+
+        //property
+
+        public string FilePath
+        {
+            get => filePath;
+            set => filePath = value;
+        }
+        public bool Modificato
+        {
+            get => modificato;
+            set => modificato = value;
+        }
+
+        //costruttore
+
+        public clsFile()
+        {
+            FilePath = "";
+            Modificato = false;
+        }
+
+        //metodi pubblici
+
+        public void apri()
+        {
+            OpenFileDialog dlgApri = new OpenFileDialog();
+            dlgApri.Filter = "Documento|*.rtf|Tutti i file|*.*";
+            dlgApri.Title = "AlbyPad - Apri";
+            dlgApri.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (dlgApri.ShowDialog() == DialogResult.OK)           
+                FilePath = dlgApri.FileName;          
+        }
+
+        public bool chiediSalvaConNome()
+        {
+            bool isOk;
+
+            SaveFileDialog dlgSalva = new SaveFileDialog();
+
+            dlgSalva.Filter = "Documento|*.rtf;|Tutti i file|*.*";
+            dlgSalva.AddExtension = true;
+            dlgSalva.Title = "AlbyPad - Salva";
+            dlgSalva.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (dlgSalva.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = dlgSalva.FileName;
+                isOk = true;
+            }
+            else
+                isOk = false;
+
+            return isOk;
+        }
+        
+        public string getFileName()
+        {
+            //ritorna percorso completo
+            string s = "";
+
+            if (!string.IsNullOrEmpty(FilePath))          
+                s = Path.GetFileName(FilePath);
+      
+            return filePath == "" ? "Senza nome" : s;
+        }
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newRepoGonellaAlberto
+{
+    public partial class frmAlbyPad : Form
+    {
+        clsFile fileManager;
+
+        public frmAlbyPad()
+        {
+            InitializeComponent();
+            fileManager = new clsFile();
+        }
+
+        #region richiami funzioni
+
+        private void apriToolStripMenuItem_Click(object sender, EventArgs e) => apri();
+        private void apriToolStripButton_Click(object sender, EventArgs e) => apri();
+        private void rtb_TextChanged(object sender, EventArgs e) => fileManager.Modificato = true;
+        private void salvaToolStripButton_Click(object sender, EventArgs e) => salva();
+        private void salvaToolStripMenuItem_Click(object sender, EventArgs e) => salva();
+        private void salvaconnomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileManager.Modificato)
+                salvaConNome();
+        }
+        private void frmAlbyPad_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (fileManager.Modificato)
+                if (MessageBox.Show("Vuoi salvare prima di uscire?",
+                    "Salvataggio", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    salva();
+                }
+        }
+
+        #endregion
+
+        #region implementazione funzioni
+
+        private void apri()
+        {
+            fileManager.apri();
+            rtb.LoadFile(fileManager.FilePath);
+        }
+
+        private void salva()
+        {
+            if (fileManager.Modificato)
+                if (fileManager.FilePath != "")
+                    rtb.SaveFile(fileManager.FilePath);
+                else
+                    salvaConNome();                     
+        }
+
+        private void salvaConNome()
+        {
+            if (fileManager.chiediSalvaConNome())
+                rtb.SaveFile(fileManager.FilePath);
+        }
+
+        #endregion
+    }
+}
